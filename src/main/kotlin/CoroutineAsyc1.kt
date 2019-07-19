c05b5df4ea7e075afb948cae56f2977a280d0764
yaoqianfa <yaoqianfa@lakala.com>
2019-07-19 16:57:14 +0800

    add

--- src/main/kotlin/CoroutineAsyc1.kt
+++ src/main/kotlin/CoroutineAsyc1.kt
@@ -15,7 +15,31 @@ import kotlin.system.measureTimeMillis
  * async函数启动一个协程返回一个Deferred（一个轻量级非阻塞的future），它也是一个job，附带结果值
  * 在结构化并发中启动多个协程，当有异常发生，正在执行的协程会被父线程取消
  *
+ * 注：
+ * kotlin中强烈不建议使用Async-style functions，这样会导致非结构化并发，不能取消启动的后台协程，如下：
  *
+ * // The result type of somethingUsefulOneAsync is Deferred<Int>
+ *fun somethingUsefulOneAsync() = GlobalScope.async {
+ *   doSomethingUsefulOne()
+ *}
+
+ * // The result type of somethingUsefulTwoAsync is Deferred<Int>
+ *fun somethingUsefulTwoAsync() = GlobalScope.async {
+ *  doSomethingUsefulTwo()
+ *}
+ * fun main() {
+ *    val time = measureTimeMillis {
+ *        // we can initiate async actions outside of a coroutine
+ *        val one = somethingUsefulOneAsync()
+ *        val two = somethingUsefulTwoAsync()
+ *        // but waiting for a result must involve either suspending or blocking.
+ *        // here we use `runBlocking { ... }` to block the main thread while waiting for the result
+ *        runBlocking {
+ *            println("The answer is ${one.await() + two.await()}")
+ *        }
+ *    }
+ *    println("Completed in $time ms")
+ *}
  */
 fun main() = runBlocking {
     var time = measureTimeMillis {
@@ -45,3 +69,4 @@ suspend fun doSomethingUsefulTwo(): Int {
     delay(1000L) // pretend we are doing something useful here, too
     return 29
 }
+
